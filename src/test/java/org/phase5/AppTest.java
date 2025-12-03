c8fc4665a66c98e255d139181dcf67d087c29514
Lucas Godoy <foundationer@tuta.io>
2025-12-03 15:02:33 -0300

    Added maven configuration.

---
+++ src/test/java/org/phase5/AppTest.java
@@ -0,0 +1,12 @@
+package org.phase5;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertThat(true, is(true));
+    }
+}
