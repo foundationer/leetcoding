979c2449f38d2052b1be6850ae18a7640faeafb8
Lucas Godoy <foundationer@tuta.io>
2026-02-12 14:42:05 -0300

    Added exercise: Sorted Array to height-balanced BST.

---
+++ src/main/java/org/phase5/SortedArrayToBST.java
@@ -0,0 +1,20 @@
+package org.phase5;
+
+import org.phase5.structures.tree.TreeNode;
+
+public class SortedArrayToBST {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if(left > right) return null;
+
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, left, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, right);
+        return root;
+    }
+}
